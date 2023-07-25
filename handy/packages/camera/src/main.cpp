eacbfce44d8e7b1ef3e6b63b0ca3d955ddb4dfe0
dDenis Bakin <denis.bakin5@gmail.com>
2023-07-25 12:46:20 +0000

    namespace handy added

--- handy/packages/camera/src/main.cpp
+++ handy/packages/camera/src/main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[]) {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<CameraNode>());
+    rclcpp::spin(std::make_shared<handy::CameraNode>());
     rclcpp::shutdown();
 
     return 0;
