6aaf785a57579a93927563fa6054d92771c4ad54
Denis Bakin <denis.bakin5@gmail.com>
2023-07-30 18:39:42 +0000

    Deleted redundant dir

--- handy/packages/camera/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <camera.h>
-
-int main(int argc, char *argv[]) {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<handy::CameraNode>());
-    rclcpp::shutdown();
-
-    return 0;
-}
