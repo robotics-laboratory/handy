78b6068d4df43075d9fa7c75a78643bea0533cdc
Denis Bakin <denis.bakin5@gmail.com>
2023-12-01 23:08:58 +0000

    PR-requested changes implemented

--- packages/camera/scripts/camera_identifier/src/identifier.cpp
+++ packages/camera/scripts/camera_identifier/src/identifier.cpp
@@ -14,41 +14,48 @@ void abortIfNot(std::string_view msg, int status) {
     }
 }
 
+void printHelp() {
+    printf("\nhelp:\n");
+
+    printf("usage: ./camera_identifier <camera ID>\n");
+    printf("<camera ID> must be 8-bit unsigned int\n");
+    printf("note that only one camera should be attached\n");
+}
+
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        printf("At least one parameter (camera ID) is required.\n");
-        return 0;
+    uint8_t required_camera_id;
+    if (argc == 2) {
+        try {
+            required_camera_id = static_cast<uint8_t>(std::stoi(argv[1]));
+        } catch (const std::exception& e) {
+            printf("%s -- failed to read required id: %s\n", e.what(), argv[1]);
+            printHelp();
+            return -1;
+        }
+    } else {
+        printHelp();
+        return -1;
     }
 
-    uint8_t required_camera_id;
-    try {
-        required_camera_id = static_cast<uint8_t>(std::stoi(argv[1]));
-    } catch (const std::exception& e) {
-        printf("%s\n", e.what());
-        return 0;
-    }
-    printf("Required camera ID is: %d\n", required_camera_id);
+    printf("required id is: %d\n", required_camera_id);
 
     abortIfNot("camera init", CameraSdkInit(0));
     int camera_num = 100;  // attach all connected cameras
     tSdkCameraDevInfo cameras_list[100];
     abortIfNot("camera listing", CameraEnumerateDevice(cameras_list, &camera_num));
     if (camera_num != 1) {
-        printf("More than 2 cameras are attached. ID assignment is ambiguous.\n");
-        return 0;
+        printf("more than 2 cameras are attached, assignment is ambiguous\n");
+        printHelp();
+        return -1;
     }
 
     int camera_handle;
     abortIfNot("camera init", CameraInit(cameras_list, -1, -1, &camera_handle));
 
     uint8_t prev_id;
-    abortIfNot("obtaining previous ID", CameraLoadUserData(camera_handle, 0, &prev_id, 1));
-    printf("Obtained previous camera ID (may be undefined): %d\n", prev_id);
-
-    abortIfNot("setting new ID", CameraSaveUserData(camera_handle, 0, &required_camera_id, 1));
-
-    abortIfNot("obtaining newly set ID", CameraLoadUserData(camera_handle, 0, &prev_id, 1));
-    printf("Set new camera ID: %d\n", prev_id);
+    abortIfNot("get prev camera_id", CameraLoadUserData(camera_handle, 0, &prev_id, 1));
+    printf("prev id=%d\n", prev_id);
+    abortIfNot("set new camera_id", CameraSaveUserData(camera_handle, 0, &required_camera_id, 1));
 
     abortIfNot("camera uninit", CameraUnInit(camera_handle));
 
