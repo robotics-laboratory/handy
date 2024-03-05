9614794df5a89d8457e3129805d72252ba895c60
Bakin Denis <denis.bakin5@gmail.com>
2024-03-05 20:07:11 +0000

    feat: separated id-tool from camera package

--- lint.bash
+++
@@ -1,6 +0,0 @@
-FILES=$(find . \( -name '*.h' -or -name '*.cpp' -or -name '*.cc' \) -not -path '*/Telegram/*' -not -path '*/cmake/*' -not -path '*/lib/*')
-          echo "Files to be linted:"
-          echo "$FILES"
-          for FILE in $FILES; do
-            clang-tidy -p=$1 $FILE
-          done
