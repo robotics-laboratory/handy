9b9eb70e52d328f3af5477aa9e4ae73de04ca8c0
Daniel Paleyev <danny.paleyev@gmail.com>
2024-03-29 22:37:46 +0300

    fix: gpu support

--- detection/inference.py
+++ detection/inference.py
@@ -26,7 +26,8 @@ if __name__ == "__main__":
     checkpoint = torch.load(args.checkpoint, map_location=torch.device('cpu'))
     model.load_state_dict({k[6:]: v for k, v in checkpoint["state_dict"].items() if k.startswith("model.")})
 
-    model.eval()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.eval().to(device)
 
     width = args.size
     height = args.size
@@ -48,6 +49,8 @@ if __name__ == "__main__":
         image_input = np.transpose(image, (2, 0, 1)).astype(np.float32)
         image_input = torch.tensor(image_input, dtype=torch.float)
         image_input = torch.unsqueeze(image_input, 0)
+
+        image_input = image_input.to(device)
 
         outputs = model(image_input)
 
