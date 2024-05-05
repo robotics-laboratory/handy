80075db2266721b4dc8ef770334cee98580ebe2b
Daniel Paleyev <danny.paleyev@gmail.com>
2024-05-05 14:55:34 +0300

    feat: dataset for sequence of images

--- training/localisation/datasets.py
+++ training/localisation/datasets.py
@@ -6,12 +6,11 @@ import torch
 import argparse
 import numpy as np
 import lightning as L
-from torchvision.transforms import v2
+import albumentations as A
 
 
 from torch.utils.data import Dataset
-from torchvision import tv_tensors
-from torchvision.io import read_image
+from albumentations.pytorch import ToTensorV2
 
 class Denormalize():
     def __init__(self, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), p=1.0):
@@ -26,46 +25,55 @@ class Denormalize():
         return img
 
 
-def get_train_transform(width=320, height=192):
-    return v2.Compose([
-        #TODO: Add bbox safe random crop
-        v2.Resize(size=(height, width), antialias=True),
-        v2.RandomHorizontalFlip(p = 0.5),
-        v2.RandomVerticalFlip(p = 0.5),
-        v2.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
-        v2.ToDtype(torch.float32, scale=True),
-        v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
+def get_train_transform():
+    return A.ReplayCompose([
+        A.HorizontalFlip(p=0.5),
+        A.VerticalFlip(p=0.5),
+        A.RandomBrightnessContrast(p=0.3),
+        A.Normalize(mean=(0.077, 0.092, 0.142), std=(0.068, 0.079, 0.108), max_pixel_value=1, p=1.0),
+        ToTensorV2(p=1.0),
+    ], bbox_params={
+        'format': 'pascal_voc',
+        'label_fields': ['labels']
+    })
 
-
-def get_valid_transform(width=320, height=192):
-    return v2.Compose([
-        v2.Resize(size=(height, width), antialias=True),
-        v2.ToDtype(torch.float32, scale=True),
-        v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
+def get_valid_transform():
+    return A.ReplayCompose([
+        A.Normalize(mean=(0.077, 0.092, 0.142), std=(0.068, 0.079, 0.108), max_pixel_value=1, p=1.0),
+        ToTensorV2(p=1.0),
+    ], bbox_params={
+        'format': 'pascal_voc', 
+        'label_fields': ['labels']
+    })
 
 class LocalisationDataset(Dataset):
-    def __init__(self, image_dir, annot_file, transforms, train=True):
+    def __init__(self, image_dir, annot_file, width, height, n_last = 5, transforms=None):
+
         self.image_dir = image_dir
         self.annot_file = annot_file
+        self.width = width
+        self.height = height
+        self.n_last = n_last
         self.transforms = transforms
-        images = []
         self.bboxes = json.load(open(annot_file))
 
+        images = []
         for file in os.listdir(self.image_dir):
-            if file.endswith('.png'):
+            if file.endswith('.png') and file in self.bboxes:
                 images.append(file)
         
         images.sort()
-        train_images, test_images = sklearn.model_selection.train_test_split(images, train_size=0.8, random_state=42)
-        self.images = train_images if train else test_images
+        self.images = images
     
     def __getitem__(self, index):
         image_name = self.images[index]
         image_path = os.path.join(self.image_dir, image_name)
 
-        image = read_image(image_path)
+
+        image_num = int(image_name[-8:-4])
+        image_prefix = image_name[:-8]
+
+        resized_images = []
 
         bboxes = self.bboxes[image_name]
         xmin = bboxes['xmin']
@@ -73,17 +81,61 @@ class LocalisationDataset(Dataset):
         xmax = bboxes['xmax']
         ymax = bboxes['ymax']
 
-        bboxes_resized = [[xmin, ymin, xmax, ymax]]
+        for i in range(image_num, image_num - self.n_last, -1):
 
-        target = {}
-        boxes = tv_tensors.BoundingBoxes(bboxes_resized, format='XYXY', canvas_size=image.shape[-2:])
+            image_name = f"{image_prefix}{str(i).rjust(4, '0')}.png"
+            image_path = os.path.join(self.image_dir, image_name)
+
+            image = cv2.imread(image_path)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+            image_resized = cv2.resize(image, (self.width, self.height))
+            image_resized /= 255.0
+
+            resized_images.append(image_resized)
+
+        
+        image_height, image_width, _ = image.shape
+        xmin_resized = int(xmin * self.width / image_width)
+        ymin_resized = int(ymin * self.height / image_height)
+        xmax_resized = int(xmax * self.width / image_width)
+        ymax_resized = int(ymax * self.height / image_height)
+
+        if ymin_resized == ymax_resized:
+            if ymin_resized == 0:
+                ymax_resized += 1
+            else:
+                ymin_resized -= 1
+        if xmin_resized == xmax_resized:
+            if xmin_resized == 0:
+                xmax_resized += 1
+            else:
+                xmin_resized -= 1
+
+        bboxes_resized = torch.as_tensor([[xmin_resized, ymin_resized, xmax_resized, ymax_resized]], dtype=torch.int64)
+        labels = torch.as_tensor([1], dtype=torch.int64)
+
+        aug_images = []
+
         if self.transforms:
-            image, boxes_out = self.transforms(image, boxes)
-            box = boxes_out[0]
-            target['ball_center'] = [int((box[0] + box[2]) / 2), int((box[1] + box[3]) / 2)]
-            target['ball_rad'] = (box[2] - box[0] + box[3] - box[1]) / 4
+            data = self.transforms(image=resized_images[0], bboxes=bboxes_resized, labels=labels, return_replay=True)
+            bboxes_resized = data['bboxes']
+            replay = data['replay']
+            aug_images.append(data['image'])
 
-        return image, target
+            for i in range(1, self.n_last):
+                data = A.ReplayCompose.replay(replay, image=resized_images[i], bboxes=bboxes_resized, labels=labels)
+                aug_images.append(data['image'])
+            
+            resized_images = aug_images
+        
+        target = {}
+        box = bboxes_resized[0]
+        target['ball_center'] = [int((box[0] + box[2]) / 2), int((box[1] + box[3]) / 2)]
+        target['ball_rad'] = (box[2] - box[0] + box[3] - box[1]) / 4
+
+        stack = torch.cat(resized_images, dim=0)
+
+        return stack, target
 
     def __len__(self):
         return len(self.images)
@@ -95,19 +147,22 @@ def collate_fn(batch):
 
 
 class LocalisationDataModule(L.LightningDataModule):
-    def __init__(self, data_dir, annot_file, width=320, height=192, batch_size=32):
+    def __init__(self, data_dir, annot_file, width=320, height=192, n_last=5,  batch_size=32):
         super().__init__()
         self.data_dir = data_dir
         self.annot_file = annot_file
         self.width = width
         self.height = height
+        self.n_last = n_last
         self.batch_size = batch_size
     
     def setup(self, stage):
-        self.train_dataset = LocalisationDataset(self.data_dir, self.annot_file,
-                                              transforms=get_train_transform(self.width, self.height), train=True)
-        self.valid_dataset = LocalisationDataset(self.data_dir, self.annot_file,
-                                              transforms=get_valid_transform(self.width, self.height), train=False)
+        self.train_dataset = LocalisationDataset(os.path.join(self.train_dir, 'images_rgb'), os.path.join(self.train_dir, 'boxes.json'),
+                                              self.width, self.height, self.n_last,
+                                              transforms=get_train_transform())
+        self.valid_dataset = LocalisationDataset(os.path.join(self.val_dir, 'images_rgb'), os.path.join(self.val_dir, 'boxes.json'),
+                                              self.width, self.height, self.n_last,
+                                              transforms=get_valid_transform())
     
     def train_dataloader(self):
         return torch.utils.data.DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=2, collate_fn=collate_fn, persistent_workers=True)
@@ -118,32 +173,3 @@ class LocalisationDataModule(L.LightningDataModule):
     def transfer_batch_to_device(self, batch, device, dataloader_idx):
         images, targets = batch
         return images.to(device), targets
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Localisation Dataset Parameters')
-    parser.add_argument('--image_dir', type=str, default='bgr_image_undistort', help='Path to the directory containing the images')
-    parser.add_argument('--annot_file', type=str, default='datasets/annotated_12_02_23/bounding_boxes.json', help='Path to the file containing the bounding box annotations')
-    parser.add_argument('--width', type=int, default=320, help='The width to which the images will be resized')
-    parser.add_argument('--height', type=int, default=192, help='The height to which the images will be resized')
-    parser.add_argument('--train', action='store_true', help='Flag indicating whether to train the model')
-
-    args = parser.parse_args()
-
-    transform = get_train_transform() if args.train else get_valid_transform()
-    dataset = LocalisationDataset(args.image_dir, args.annot_file, transform, train=args.train)
-    
-    print(f"Number of samples in the train dataset: {len(dataset)}")
-
-    def visualize(image, target):
-        image = Denormalize()(image.permute(1, 2, 0).numpy())
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        center = target['ball_center']
-        cv2.circle(image, center, 1, (255, 0, 0), -1)
-        cv2.imshow('image', image)
-        cv2.waitKey(0)
-    
-    for i in range(5):
-        image, target = dataset[i]
-        visualize(image, target)
-    cv2.destroyAllWindows()
-
