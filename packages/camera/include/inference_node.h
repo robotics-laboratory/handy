c03917aedfcb60f2eebafa20f50ccccb15bfb38e
Denis Bakin <denis.bakin5@gmail.com>
2025-04-27 21:37:26 +0300

    feat: complete inference node, compilation todo: sample test and test CUDA API calls if needed

--- packages/camera/include/inference_node.h
+++ packages/camera/include/inference_node.h
@@ -1,8 +1,9 @@
 #pragma once
 
-#include "camera.h"      
+#include "camera.h"
 #include <torch/script.h>
 #include <opencv2/core.hpp>
+#include "c10/cuda/CUDAStream.h"
 #include <boost/lockfree/queue.hpp>
 #include <mutex>
 #include <atomic>
@@ -12,33 +13,37 @@
 #include <vector>
 #include <functional>
 #include <memory>
+#include <array>
 
 namespace handy::camera {
 
 class BallInferenceNode {
   public:
+    static const int kMaxCameraNum = 2;
+
     /// Per‐frame detection result
     struct DetectionOutput {
-        std::vector<cv::Rect> bboxes;  // one bbox per camera image
-        std::vector<bool> flags;       // true = needs segmentation for that camera
-        uint32_t timestamp;
+        std::array<cv::Point, kMaxCameraNum> centers;  // one ball center per camera image
+        std::array<bool, kMaxCameraNum> flags;         // true = needs segmentation for that camera
+        uint32_t timestamp = 0;
+        SynchronizedFrameBuffers* images_to_segment = nullptr;
     };
 
     /// Per‐frame segmentation result
     struct SegmentationOutput {
-        // one mask per camera image that requested segmentation
-        std::vector<at::Tensor> masks;
+        // one centroid per camera image that requested segmentation
+        std::vector<cv::Point> centroids;
         std::vector<int> camera_indices;
-        uint32_t timestamp;
+        uint32_t timestamp = 0;
     };
 
-    using DetectionCallback = std::function<void(std::shared_ptr<DetectionOutput>)>;
-    using SegmentationCallback = std::function<void(std::shared_ptr<SegmentationOutput>)>;
+    using DetectionCallback = std::function<void(DetectionOutput)>;
+    using SegmentationCallback = std::function<void(SegmentationOutput)>;
 
     /// ctor loads both models and sets up queues/threads
     BallInferenceNode(
         const std::string& det_model_path, const std::string& seg_model_path, Size input_size,
-        int window_size = 5);
+        std::function<void(SynchronizedFrameBuffers*)> deleter, int window_size = 5);
     ~BallInferenceNode();
 
     /// Subscribe to detection outputs
@@ -54,37 +59,53 @@ class BallInferenceNode {
     /// Signal all threads to stop, join them, and release resources
     void stop();
 
+    void handleSyncBuffer(std::shared_ptr<SynchronizedFrameBuffers> buffer);
+
   private:
+    void detectSingleCamera(const std::deque<at::Tensor>& frames, DetectionOutput& detect_result,
+        int camera_idx, c10::cuda::CUDAStream& infer_stream);
     void detectionLoop();
     void segmentationLoop();
+
 
     // === model objects ===
     torch::jit::script::Module det_model_;
     torch::jit::script::Module seg_model_;
 
-    // === input queue & rolling window ===
-    boost::lockfree::queue<std::shared_ptr<SynchronizedFrameBuffers>> det_queue_{128};
-    const int window_size_;
-    Size input_size_;
+    std::function<void(SynchronizedFrameBuffers*)> buffer_deleter_;
 
-    // protected by ring_mutex_
-    std::deque<at::Tensor> ring_buffer_;
-    std::mutex ring_mutex_;
+    struct Param {
+        // === input queue & rolling window ===
+        int window_size = 5;
+        Size input_size{320, 192};
+        const at::Tensor means =
+            torch::tensor({0.077, 0.092, 0.142}).view({1, 3, 1, 1}).to(at::kFloat);
+        const at::Tensor stds =
+            torch::tensor({0.068, 0.079, 0.108}).view({1, 3, 1, 1}).to(at::kFloat);
+    } param_;
 
-    // === intermediate queues ===
-    boost::lockfree::queue<DetectionOutput> seg_queue_{128};
+    struct State {
+        boost::lockfree::queue<SynchronizedFrameBuffers*> det_queue{64};
 
-    // === subscriber callbacks ===
-    std::vector<DetectionCallback> det_cbs_;
-    std::vector<SegmentationCallback> seg_cbs_;
-    std::mutex cb_mutex_;
+        // protected by ring_mutex_
+        std::array<std::deque<at::Tensor>, kMaxCameraNum> ring_buffer;
+        std::mutex ring_mutex;
 
-    // === threading & synchronization ===
-    std::thread det_thread_;
-    std::thread seg_thread_;
-    std::atomic<bool> running_{false};
-    std::condition_variable seg_cv_;
-    std::mutex seg_mutex_;
+        // === intermediate queues ===
+        boost::lockfree::queue<DetectionOutput> seg_queue{128};
+
+        // === subscriber callbacks ===
+        std::vector<DetectionCallback> det_cbs;
+        std::vector<SegmentationCallback> seg_cbs;
+        std::mutex cb_mutex;
+
+        // === threading & synchroization ===
+        std::atomic<bool> running{false};
+        std::thread det_thread;
+        std::thread seg_thread;
+        std::condition_variable seg_cv;
+        std::mutex seg_mutex;
+    } state_;
 };
 
 }  // namespace handy::camera
---
+++ packages/camera/src/inference_node.cpp
@@ -0,0 +1,276 @@
+#include "inference_node.h"
+#include <opencv2/imgproc.hpp>
+#include <torch/torch.h>
+#include <torch/types.h>
+#include "c10/cuda/CUDAStream.h"
+#include "c10/cuda/CUDAGuard.h"
+#include <ATen/cuda/CUDAContext.h>
+
+namespace handy::camera {
+
+BallInferenceNode::BallInferenceNode(
+    const std::string& det_model_path, const std::string& seg_model_path, Size input_size,
+    std::function<void(SynchronizedFrameBuffers*)> deleter, int window_size) {
+    det_model_ = torch::jit::load(det_model_path);
+    det_model_.eval();
+    det_model_.to(at::kCUDA);
+
+    seg_model_ = torch::jit::load(seg_model_path);
+    seg_model_.eval();
+    seg_model_.to(at::kCUDA);
+
+    // Store params
+    param_.input_size = input_size;
+    param_.window_size = window_size;
+    buffer_deleter_ = deleter;
+}
+
+BallInferenceNode::~BallInferenceNode() { stop(); }
+
+void BallInferenceNode::registerDetectionSubscriber(const DetectionCallback& cb) {
+    std::lock_guard<std::mutex> lk(state_.cb_mutex);
+    state_.det_cbs.push_back(cb);
+}
+
+void BallInferenceNode::registerSegmentationSubscriber(const SegmentationCallback& cb) {
+    std::lock_guard<std::mutex> lk(state_.cb_mutex);
+    state_.seg_cbs.push_back(cb);
+}
+
+void BallInferenceNode::handleSyncBuffer(std::shared_ptr<SynchronizedFrameBuffers> buffer) {
+    while (!state_.det_queue.push(buffer.get())) {
+    }
+}
+
+void BallInferenceNode::start() {
+    state_.running = true;
+    // Launch threads
+    state_.det_thread = std::thread(&BallInferenceNode::detectionLoop, this);
+    state_.seg_thread = std::thread(&BallInferenceNode::segmentationLoop, this);
+}
+
+void BallInferenceNode::stop() {
+    bool expected = true;
+    if (state_.running.compare_exchange_strong(expected, false)) {
+        state_.seg_cv.notify_one();
+        if (state_.det_thread.joinable()) {
+            state_.det_thread.join();
+        }
+        if (state_.seg_thread.joinable()) {
+            state_.seg_thread.join();
+        }
+    }
+}
+
+void BallInferenceNode::detectSingleCamera(
+    const std::deque<at::Tensor>& frames, DetectionOutput& detect_result, int camera_idx,
+    c10::cuda::CUDAStream& infer_stream) {
+    std::vector<at::Tensor> frames_vec(frames.begin(), frames.end());
+    at::Tensor batch = at::cat(frames_vec, 0);  // dim=0
+    at::Tensor gpu_in = batch.to(at::Device(at::kCUDA), /*non_blocking=*/true);
+
+    torch::NoGradGuard no_grad;
+    at::cuda::CUDAStreamGuard guard(infer_stream);
+    auto out_tuple = det_model_.forward({gpu_in}).toTuple();
+
+    // Unpack model outputs
+    //    out_tuple[0]: prob distribution (1, width + height)
+    //    out_tuple[1]: presence logits (1, 2)
+    at::Tensor center_distr = out_tuple->elements()[0].toTensor();
+    at::Tensor success_detection_distr = out_tuple->elements()[1].toTensor();
+
+    // Threshold & argmax to get (x,y) center
+    //    replicate: pred[pred < thresh]=0, then argmax on slices
+    const float thresh = 0.0001;
+    int W = param_.input_size.width;
+    center_distr = center_distr.clone();
+    center_distr.masked_fill_(center_distr < thresh, 0.0f);
+
+    at::Tensor prob_x = center_distr.slice(1, 0, W);                     // {1, W}
+    at::Tensor prob_y = center_distr.slice(1, W, center_distr.size(1));  // {1, H}
+
+    int64_t pred_x = prob_x.argmax(1).item<int64_t>();
+    int64_t pred_y = prob_y.argmax(1).item<int64_t>();
+    detect_result.centers[camera_idx] = {pred_x, pred_y};
+
+    // 4. Determine flag from presence logits
+    int64_t pres_idx = success_detection_distr.argmax(1).item<int64_t>();
+    detect_result.flags[camera_idx] = (pres_idx == 1);  // class 1 means object present
+}
+
+void BallInferenceNode::detectionLoop() {
+    auto infer_stream = at::cuda::getStreamFromPool();
+    std::vector<std::deque<at::Tensor>> rollBuf(kMaxCameraNum);
+
+    SynchronizedFrameBuffers* sync;
+    while (state_.running) {
+        if (!state_.det_queue.pop(sync)) {
+            std::this_thread::sleep_for(1ms);
+            continue;
+        }
+
+        DetectionOutput detection_out;
+        bool any_successful_detection = false;
+        // Preprocess each camera image
+        for (size_t cam = 0; cam < sync->images.size(); ++cam) {
+            uint8_t* raw = sync->images[cam];
+            Size sz = sync->image_sizes[cam];
+            cv::Mat bayer(sz.height, sz.width, CV_8UC1, raw);
+
+            static thread_local cv::Mat rgb;
+            cv::cvtColor(bayer, rgb, cv::COLOR_BayerBG2RGB);
+
+            static thread_local cv::Mat resized;
+            cv::resize(rgb, resized, cv::Size(param_.input_size.width, param_.input_size.height));
+            // To tensor  of shape (1, height, width, channel) and perform all the preprocessing
+            at::Tensor t =
+                torch::from_blob(resized.data, {1, resized.rows, resized.cols, 3}, at::kByte)
+                    .permute({0, 3, 1, 2})  // (1, height, width, channel)
+                    .toType(at::kFloat)
+                    .div_(255.0)  // normalize
+                    .sub_(param_.means)
+                    .div_(param_.stds)
+                    .pin_memory();  // for async copy
+
+            rollBuf[cam].push_back(t);
+            if (rollBuf[cam].size() > (size_t)param_.window_size) {
+                rollBuf[cam].pop_front();
+            }
+            if (rollBuf[cam].size() < (size_t)param_.window_size) {
+                continue;
+            }
+            detectSingleCamera(rollBuf[cam], detection_out, cam, infer_stream);
+            any_successful_detection |= detection_out.flags[cam];
+        }
+        detection_out.images_to_segment = nullptr;
+        detection_out.timestamp = sync->timestamp;
+
+        {
+            std::lock_guard<std::mutex> lk(state_.cb_mutex);
+            for (auto& cb : state_.det_cbs) {
+                cb(detection_out);
+            }
+        }
+
+        if (any_successful_detection) {
+            // segmentation requires initial images, so we need to send pointer to buffer too
+            detection_out.images_to_segment = sync;
+            state_.seg_queue.push(detection_out);
+            state_.seg_cv.notify_one();
+        } else {
+            // in case no segmentation is required we can release buffer
+            buffer_deleter_(sync);
+        }
+    }
+}
+
+void BallInferenceNode::segmentationLoop() {
+    while (state_.running) {
+        std::unique_lock<std::mutex> lk(state_.seg_mutex);
+        state_.seg_cv.wait_for(
+            lk, 5ms, [this]() { return !state_.running || !state_.seg_queue.empty(); });
+        if (!state_.running && state_.seg_queue.empty()) break;
+
+        DetectionOutput det;
+        if (!state_.seg_queue.pop(det)) {
+            continue;
+        }
+
+        auto sync = det.images_to_segment;
+        if (!sync) {
+            continue;
+        }
+
+        std::vector<at::Tensor> masks;
+        std::vector<int> cams;
+        std::vector<cv::Point> centers;
+        std::vector<cv::Point2f> centroids;
+
+        SegmentationOutput segmentation_output;
+        segmentation_output.timestamp = det.timestamp;
+
+        // Process each flagged camera
+        for (size_t cam = 0; cam < det.flags.size(); ++cam) {
+            if (!det.flags[cam]) continue;
+            // 1) Convert Bayer to RGB
+            uint8_t* raw = sync->images[cam];
+            Size sz = sync->image_sizes[cam];
+            cv::Mat bayer(sz.height, sz.width, CV_8UC1, raw);
+            cv::Mat rgb;
+            cv::cvtColor(bayer, rgb, cv::COLOR_BayerBG2RGB);
+
+            // 2) Crop centered square of size cropSize
+            const int cropSize = 128;
+            int half = cropSize / 2;
+            cv::Point pt = det.centers[cam];
+            int x = std::clamp(pt.x, half, sz.width - half);
+            int y = std::clamp(pt.y, half, sz.height - half);
+            int x0 = x - half;
+            int y0 = y - half;
+            cv::Rect cropRect(x0, y0, cropSize, cropSize);
+            cv::Mat crop = rgb(cropRect);
+
+            at::Tensor t =
+                torch::from_blob(crop.data, {1, crop.rows, crop.cols, 3}, at::kByte)
+                    .permute({0, 3, 1, 2})
+                    .toType(at::kFloat)
+                    .div_(255.0)
+                    .sub_(param_.means)
+                    .div_(param_.stds)
+                    .pin_memory();
+
+            at::Tensor gpu_in = t.to(at::kCUDA, /*non_blocking=*/true);
+            torch::NoGradGuard ndg;
+            at::Tensor out = seg_model_.forward({gpu_in}).toTensor().cpu();
+            // out shape {1, 1, cropSize, cropSize}
+            at::Tensor prob = out.sigmoid().squeeze(0).squeeze(0);  // {H, W} on CUDA
+
+            constexpr float thresh = 0.5f;
+            at::Tensor bin = prob > thresh;  // ByteTensor {H, W} on CUDA
+            at::Tensor coords = bin.nonzero();
+
+            // compute centroid as the mean of these coords
+            at::Tensor centroid;
+            if (coords.numel() == 0) {
+                // {row_center, col_center}
+                centroid =
+                    torch::tensor({cropSize / 2.0, cropSize / 2.0}, at::kFloat).to(at::kCUDA);
+            } else {
+                // convert coords to float and take mean over dim=0 → {mean_row, mean_col}
+                centroid = coords.to(at::kFloat).mean(0);
+            }
+
+            centroid = centroid.to(at::kCPU);
+
+            // 5) Extract values
+            float centroid_y = centroid[0].item<float>() + y0;
+            float centroid_x = centroid[1].item<float>() + x0;
+
+            segmentation_output.centroids.emplace_back(centroid_x, centroid_y);
+            segmentation_output.camera_indices.push_back(cam);
+        }
+
+        if (segmentation_output.camera_indices.empty()) {
+            continue;
+        }
+
+        std::lock_guard<std::mutex> lk_callback(state_.seg_mutex);
+        for (auto& cb : state_.seg_cbs) {
+            cb(segmentation_output);
+        }
+
+        // now we do not need buffer
+        buffer_deleter_(sync);
+    }
+}
+
+}  // namespace handy::camera
+
+int main() {
+    handy::camera::BallInferenceNode node(
+        "detection.pt",
+        "segmentation.pt",
+        {320, 192},
+        [](handy::camera::SynchronizedFrameBuffers* buf) { std::cout << buf << '\n'; });
+    return 0;
+}
